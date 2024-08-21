3b5c79f51e6d082eccdc6f55821ae276e02852e6
Mathias Fogolin <mathifogo@gmail.com>
2024-08-21 10:12:22 -0300

    Añadi las partes del avanzado del tp de luca rodon Pollo

--- Tp/script.js
+++ Tp/script.js
@@ -1,4 +1,6 @@
 let lista = [];
+let numero = [];
+let posicion = [];
 let cantidadJugadoresRestantes = 26
 
 
@@ -8,17 +10,20 @@ let cantidadJugadoresRestantes = 26
 
 
 
-function agregar(jugador)
+function agregar(jugador, number, pos)
 
 {
     
 if(cantidadJugadoresRestantes>0)
 {
+number = document.getElementById("numero")
+pos = document.getElementById("posicion")
 jugador = document.getElementById("jugador");
 lista.push(jugador.value);
-console.log(lista);
+numero.push(number.value);
+posicion.push(pos.value);
 cantidadJugadoresRestantes--;
-console.log(lista.lenght);
+
 }
 
 else
@@ -44,7 +49,8 @@ alert('Se ha alcanzado los 26 jugadores');
 function limpiar()
 
 {
-
+numero = numero.splice(0, numero.length);
+posicion = posicion.splice(0, posicion.lenght);
 lista = lista.splice(0, lista.lenght); 
 cantidadJugadoresRestantes = 26
 
@@ -61,7 +67,7 @@ let convocados = document.getElementById("convocados");
 convocados.innerHTML = "";
 lista.forEach((lista) => {
     const li = document.createElement("li");
-    li.textContent = lista;
+    li.textContent = lista + " pos:" + posicion + " num:" + numero;
     convocados.appendChild(li);
 
 
